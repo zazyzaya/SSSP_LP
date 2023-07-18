40854eb1646098db3fb5b2e4823c385a43a42b9f
Isaiah <zazyzaya@gmail.com>
2023-07-18 12:54:29 -0400

    Got it working. Failure. Doesn't distinguish between benign/anom. Pack it up

--- sssp.py
+++ sssp.py
@@ -1,10 +1,12 @@
 import numpy as np 
+import networkx as nx 
 import torch
+from tqdm import tqdm 
 from torch_geometric.nn import MessagePassing
 from torch_geometric.data import Data
 from torch_geometric.utils import to_dense_adj
 
-def mp_estimate(g: Data, hops: int=10) -> torch.Tensor:
+def mp_estimate(ei: torch.Tensor, hops: int=10,  num_nodes: int=-1, verbose: bool=False) -> torch.Tensor:
     '''
     Use message passing to estimate the SSSP matrix
     and return D for the graph G. 
@@ -12,23 +14,33 @@ def mp_estimate(g: Data, hops: int=10) -> torch.Tensor:
     D matrix online? If the estimate is good enough, this is
     worth looking into 
     '''
-    if g.num_nodes is None:
-        num_nodes = g.edge_index.max()+1
-    else: 
-        num_nodes = g.num_nodes 
+    if num_nodes == -1:
+        num_nodes = ei.max().item()+1
     
     x = torch.full((num_nodes, num_nodes), float('inf'), requires_grad=False)
     
     # Every node is reachable from itself 
     x[torch.arange(num_nodes), torch.arange(num_nodes)] = 0 
-    x[g.edge_index[0], g.edge_index[1]] = 1
+    x[ei[0], ei[1]] = 1
 
     mp = MessagePassing(aggr='min')
-    for _ in range(hops-1):
+    for _ in tqdm(range(hops-1), disable=not verbose):
         # Probably a more efficient way to do this
-        x = torch.min(x, mp.propagate(g.edge_index, size=None, x=x+1))
+        x = torch.min(x, mp.propagate(ei, size=None, x=x+1))
 
     return x 
+
+# NX takes a rediculously long time
+def ei_to_nx(ei):
+    # Convert to nx graph 
+    edge_list = [(ei[0][i].item(), ei[1][i].item()) for i in range(ei.size(1))]
+    g = nx.from_edgelist(edge_list, create_using=nx.DiGraph)
+    return g 
+
+def nx_djikstra(g: nx.DiGraph): 
+    # Returns sparse representation of the form 
+    # [ (nid, {neigbhor: distance}) ]
+    return [i for i in nx.shortest_path_length(g)]
 
 
 def eigen_product(g: Data, pow=10) -> torch.Tensor:
--- test_lanl.py
+++ test_lanl.py
@@ -1,10 +1,85 @@
 import glob 
+import gzip 
+import time 
 
 import torch 
+from tqdm import tqdm 
+
+from sssp import mp_estimate
 
 IN_F = '/mnt/raid1_ssd_4tb/datasets/LANL15/torch_data/'
 OUT_F = 'data/'
 N_FILES = 1392
+N_NODES = 23156
 
-def load_to_single_graph():
+@torch.no_grad()
+def remove_dupe_edges(ei, return_index=False):
+    # Produces unique "hash" for each src,dst pair
+    encoded = ei[0] + N_NODES*ei[1]
+    uq_edges_enc = encoded.unique(return_inverse=return_index)
+
+    idx = None 
+    if return_index:
+        uq_edges_enc, idx = uq_edges_enc
+
+    # Recover original values from unique tensor and return
+    new_ei = torch.stack([
+        uq_edges_enc % N_NODES, 
+        uq_edges_enc // N_NODES
+    ])
+
+    if return_index:
+        return new_ei, idx 
+    return new_ei 
+
+def get_data_split():
+    '''
+    Comparing to Ben's paper. 
+    Find 40 fully benign days, 18 mal days for tr/te split 
+    '''
+    mal_days = set()
+    red = gzip.open(IN_F + '../redteam.txt.gz', 'rt')
+
+    line = red.readline()
+    while line: 
+        ts = int(line.split(',')[0])
+        mal_days.add(ts // (60*60*24))
+        line = red.readline()
+
+    ben_days = list(set(range(58)) - mal_days)
+    mal_days = list(mal_days)
     
+    print(mal_days) 
+    return ben_days, mal_days 
+
+def build_benign_graph(): 
+    benign, _ = get_data_split()
+    b_hours = sum([ [r+(i*24) for r in range(24)] for i in benign ], [])
+
+    ei = torch.load(IN_F + f'{b_hours.pop()}.pt').edge_index
+    ei = remove_dupe_edges(ei)
+
+    for hour in tqdm(b_hours): 
+        new_ei = torch.load(IN_F + f'{hour}.pt').edge_index
+        ei = remove_dupe_edges(torch.cat([ei, new_ei], dim=1))
+    
+    print('Saving')
+    torch.save(ei, OUT_F + 'benign.pt')
+    return ei 
+
+def test_one_hour(benign_assp, hour):
+    # Remove infinities
+    benign_assp += 1
+    benign_assp[benign_assp == float('inf')] = 0 
+
+    mal = torch.load(IN_F + f'{hour}.pt')
+    mal_ei = mal.edge_index 
+    y = mal.y 
+    del mal  
+
+    uq_mal, idx = remove_dupe_edges(mal_ei, return_index=True)
+    dists = (benign_assp[uq_mal[0]] - benign_assp[uq_mal[1]]) ** 2
+    dists = dists.sum(dim=1).float() ** (1/2)
+
+    dists = dists[idx]
+    return dists, y 
